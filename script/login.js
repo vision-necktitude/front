499f7e3abd6164f27bead58390af8cd1b6954977
joeun-01 <jjy020121@gmail.com>
2023-12-04 14:54:17 +0900

    프로젝트 합치기

--- script/login.js
+++ script/login.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (inputID.trim() !== "" && inputPassword.trim() !== "") {            
             console.log("로그인 성공~");
             alert("로그인 성공!");            
-            window.location.href = "signup.html"; // "signup.html"은 회원가입 페이지의 경로입니다
+            window.location.href = "info.html"; // "signup.html"은 회원가입 페이지의 경로입니다
         } else {
             alert("ID와 Password를 모두 입력하세요.");
             console.log("로그인 실패~")
@@ -22,5 +22,16 @@ document.addEventListener("DOMContentLoaded", function() {
         alert("회원가입 페이지로 이동합니다.");
         window.location.href = "signup.html"; // "signup.html"은 회원가입 페이지의 경로입니다
     });
+
+    document.getElementById("find-id").addEventListener("click", function () {
+        // 회원가입 페이지로 이동
+        alert("아이디 찾기 페이지로 이동합니다.");
+        window.location.href = "findAccount.html"; // "signup.html"은 회원가입 페이지의 경로입니다
+    });
     
+    document.getElementById("find-password").addEventListener("click", function () {
+        // 회원가입 페이지로 이동
+        alert("비밀번호 찾기 페이지로 이동합니다.");
+        window.location.href = "findAccount.html"; // "signup.html"은 회원가입 페이지의 경로입니다
+    });
 });
