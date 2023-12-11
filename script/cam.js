172c45e41c2224bfa94a6f43a8e9dd3c11db57f7
joeun-01 <jjy020121@gmail.com>
2023-12-12 00:06:51 +0900

    촬영 가이드라인 추가

--- script/cam.js
+++ script/cam.js
@@ -1,3 +1,6 @@
+var time = 0;
+var count = 0;
+
 document.addEventListener("DOMContentLoaded", function() {
     // fastAPI 실행 코드
     setTimeout(() => {
@@ -12,34 +15,56 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 2000);
 
     document.getElementById("start").addEventListener("click", () => {
+        document.getElementById("guideline").style.visibility = "visible";
+
+        clearInterval(time); // 타이머 초기화
+        time = setInterval(timer, 1000);
+
         var url = "http://localhost:8000/init";
         console.log(url);
 
         fetch(url)
-            .then(response => {
+        .then(response => {
             if (!response.ok) {
                 throw new Error('API 오류!');
             }
             return response.json();
-            })
-            .catch(error => {
+        })
+        .catch(error => {
             console.log('There was a problem with the fetch operation:', error);
-            });
         });
+    });
 
     document.getElementById("stop").addEventListener("click", () => {
         var url = "http://localhost:8000/end";
         console.log(url);
 
         fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('API 오류!');
-                }
-                return response.json();
-            })
-            .catch(error => {
-                console.log('There was a problem with the fetch operation:', error);
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('API 오류!');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.log('There was a problem with the fetch operation:', error);
         });
     });
 });
+
+function timer() {
+    count++; // 타이머 선택 숫자에서 -1씩 감산함(갱신되기 때문)
+    console.log("count", count);
+    
+    document.getElementById("guideline-progress").value = count;
+    
+    if(count == 6) { 
+        clearInterval(time);	// 시간 초기화
+        console.log("시간이 완료되었습니다.");
+
+        document.getElementById("guideline").style.visibility = "hidden";
+
+        count = 0;
+        document.getElementById("guideline-progress").value = count;
+    }
+}
